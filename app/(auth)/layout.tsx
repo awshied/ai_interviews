2719c7715ed40ce5751d193339bd7ff6f835b436
unknown <vendettaktl@gmail.com>
2025-04-27 12:29:21 +0700

    setup app, file and folder structure, routing and auth page

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from "react";
+import { Toaster } from "sonner";
+
+const AuthLayout = ({ children }: { children: ReactNode }) => {
+  return (
+    <div className="auth-layout">
+      {children}
+      <Toaster />
+    </div>
+  );
+};
+
+export default AuthLayout;
